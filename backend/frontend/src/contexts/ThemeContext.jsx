9606da5dc6962d3e72614bc92e6d6f4431a458d7
Butrint Hasanaj <butrint.hasanaj@dci-student.org>
2024-08-22 14:15:46 +0200

    register&login

---
+++ backend/frontend/src/contexts/ThemeContext.jsx
@@ -0,0 +1,23 @@
+// src/contexts/ThemeContext.js
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme; // Apply the theme to the body element
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
