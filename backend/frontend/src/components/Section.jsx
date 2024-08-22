900aed173d8acc1bdec1fca23564c6348d547be6
Poliana Schuelter <polianaschuelter@polianas-mbp.speedport.ip>
2024-08-22 11:04:41 +0200

    styled the RSnavstatus tooltip and change the pictures in main page

--- backend/frontend/src/components/Section.jsx
+++ backend/frontend/src/components/Section.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 import "../style/Section.css";
+import hamburguer from "../../assets/hamburguer.webp";
+import indianFood from "../../assets/indian.webp";
+// import pizza1 from "../../assets/pizza.webp";
+import pizza2 from "../../assets/pizza2.webp";
+// import pizza3 from "../../assets/pizza3.webp";
 
 function Section() {
   return (
@@ -8,48 +13,41 @@ function Section() {
         <h3 className="love">Why Choose DelivEats Delivery?</h3>
         <div className="images">
           <div className="image-block">
-            <img
-              src="https://images.deliveryhero.io/image/fd-no/LH/li0l-listing.jpg"
-              alt="Fast Delivery"
-              className="food-image"
-            />
+            <img src={hamburguer} alt="Fast Delivery" className="food-image" />
             <p className="image-description">
-              At DelivEats, we prioritize speed without compromising quality. Our rapid delivery service ensures your
-              food arrives hot and fresh, no matter where you are in the city.
+              At DelivEats, we prioritize speed without compromising quality.
+              Our rapid delivery service ensures your food arrives hot and
+              fresh, no matter where you are in the city.
             </p>
           </div>
           <div className="image-block">
-            <img
-              src="https://pics.craiyon.com/2023-10-28/a8ea30daa44748ff9ab2391675b5eb9d.webp"
-              alt="Quality Food"
-              className="food-image"
-            />
+            <img src={indianFood} alt="Quality Food" className="food-image" />
             <p className="image-description">
-              Our wide selection of partner restaurants guarantees that you'll always find exactly what you're craving.
-              From gourmet meals to comfort food, we've got it all, delivered right to your door.
+              Our wide selection of partner restaurants guarantees that you'll
+              always find exactly what you're craving. From gourmet meals to
+              comfort food, we've got it all, delivered right to your door.
             </p>
           </div>
           <div className="image-block">
-            <img
-              src="https://storage.needpix.com/rsynced_images/food-3245765_1280.jpg"
-              alt="Special Offers"
-              className="food-image"
-            />
+            <img src={pizza2} alt="Special Offers" className="food-image" />
             <p className="image-description">
-              Don't miss out on our exclusive deals and discounts. DelivEats offers special promotions daily, making it
-              easier than ever to enjoy your favorite meals at unbeatable prices.
+              Don't miss out on our exclusive deals and discounts. DelivEats
+              offers special promotions daily, making it easier than ever to
+              enjoy your favorite meals at unbeatable prices.
             </p>
           </div>
         </div>
 
         <div className="img-text">
           <p className="text1">
-            DelivEats is committed to delivering exceptional service, ensuring that every order is handled with care.
-            From lightning-fast delivery to mouth-watering meals, we bring convenience and flavor to your doorstep. Join
-            thousands of satisfied customers and experience the DelivEats difference today!
+            DelivEats is committed to delivering exceptional service, ensuring
+            that every order is handled with care. From lightning-fast delivery
+            to mouth-watering meals, we bring convenience and flavor to your
+            doorstep. Join thousands of satisfied customers and experience the
+            DelivEats difference today!
           </p>
           <img
-            src="https://images.unsplash.com/photo-1700625915031-d2c7d472ea7b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://img.freepik.com/free-photo/busy-deliveryman-driving-yellow-scooter-while-holding-pizza-boxes_273609-31761.jpg?t=st=1724244362~exp=1724247962~hmac=75f4edefe04156b2b5eec1cba789b4dc1174eb91cb98b9cec4f7803fd77d40af&w=740"
             alt="Customer Satisfaction"
             className="footer-image"
           />
